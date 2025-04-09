cf934bc501a33ef0c3acd2b1407b1a346b86ec59
prakashmaharayt980 <108126887+prakashmaharayt980@users.noreply.github.com>
2025-04-09 16:02:04 +0545

    completed

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8000,
-  },
-  plugins: [
-    react(),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-}));
+    build: {
+      target: "esnext",
+      outDir: "dist", // Vercel uses this for static output
+      assetsDir: "assets",
+      sourcemap: false,
+      minify: "esbuild",
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react", "react-dom"],
+          },
+        },
+      },
+    },
+    define: {
+      "process.env": env, // expose .env vars
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 8000,
+    },
+  };
+});
