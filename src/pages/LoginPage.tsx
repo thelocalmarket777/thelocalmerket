9a8b8d9f22bd60430b7770e1f540c71c540b58b2
prakashmaharayt980 <108126887+prakashmaharayt980@users.noreply.github.com>
2025-04-10 12:37:06 +0545

    nice

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -365,7 +365,7 @@ const LoginPage = () => {
             <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
             <polyline points="9 22 9 12 15 12 15 22"></polyline>
           </svg>
-          NexusShop
+          Startup <sub> बजार</sub>
         </Link>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           {activeTab === 'login' && 'Welcome Back'}
